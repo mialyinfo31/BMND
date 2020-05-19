b50dbfd31fe189213a8ca6d2b7532932a019a69f
Mia <mialy.info@gmail.com>
2020-05-19 12:43:12 +0200

    ecran d accueil ajouté

--- BMND/app/src/main/java/com/e/bmnd/ConnexionActivity.java
+++ BMND/app/src/main/java/com/e/bmnd/ConnexionActivity.java
@@ -3,16 +3,12 @@ package com.e.bmnd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ConnexionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClick(View view) {
+        setContentView(R.layout.activity_connexion);
     }
 }
